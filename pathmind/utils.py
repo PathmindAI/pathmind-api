b054859bb707d3ea4915a25a6b28a0e5a4cd9afc
Johnny Davenport <johnny@pathmind.com>
2021-10-12 12:52:22 -0700

    cleanup

--- pathmind/utils.py
+++ pathmind/utils.py
@@ -13,46 +13,45 @@ def write_observation_yaml(simulation: Simulation, file_path) -> None:
 
 def rollout_episodes(simulation, policy_file=None, server_url=None, debug_mode=True, episodes=1):
     pp = pprint.PrettyPrinter(indent=4)
-
     debug_mode = False if episodes > 1 else debug_mode
     done = False
     simulation.reset()
     agents = range(simulation.number_of_agents())
 
+    for episode in range(episodes):
+        print(f"Episode : {episode + 1}")
+        step = 0
+        while True:
+            if debug_mode:
+                print("----------------------------")
+                print(f"Step : {step + 1}")
+                print("----------------------------")
+                pp.pprint({"Initial observations": {f"agent_{agent_id}": simulation.get_observation(agent_id) for agent_id in agents}})
 
-    step = 0
-    while True:
-        if debug_mode:
-            print("----------------------------")
-            print(f"step : {step + 1}")
-            print("----------------------------")
-            pp.pprint({"initial observations": {f"agent_{agent_id}": simulation.get_observation(agent_id) for agent_id in agents}})
+            if policy_file:
+                actions = local_action(simulation, policy_file)
+            elif server_url:
+                actions = server_action(simulation, server_url)
+            else: #random action
+                choices = simulation.action_space(0).choices
+                size = simulation.action_space(0).size
+                actions = np.random.randint(choices, size=(len(agents), size))
 
-        if policy_file:
-            actions = local_action(simulation, policy_file)
-        elif server_url:
-            actions = server_action(simulation, server_url)
-        else: #random action
-            choices = simulation.action_space(0).choices
-            size = simulation.action_space(0).size
-            actions = np.random.randint(choices, size=(len(agents), size))
+            simulation.action = actions
 
-        simulation.action = actions
+            simulation.step()
+            dones = {f"agent_{agent_id}": simulation.is_done(agent_id) for agent_id in agents}
+            if debug_mode:
+                pp.pprint({"Actions": {f"agent_{agent_id}": simulation.action[agent_id] for agent_id in agents}})
+                pp.pprint({"Rewards": {f"agent_{agent_id}": simulation.get_reward(agent_id) for agent_id in agents}})
+                pp.pprint({"Next observations": {f"agent_{agent_id}": simulation.get_observation(agent_id) for agent_id in agents}})
+                pp.pprint({"Done": dones})
+            step += 1
 
-        simulation.step()
-        dones = {f"agent_{agent_id}": simulation.is_done(agent_id) for agent_id in agents}
-        if debug_mode:
-            pp.pprint({"actions": {f"agent_{agent_id}": simulation.action[agent_id] for agent_id in agents}})
-            pp.pprint({"rewards": {f"agent_{agent_id}": simulation.get_reward(agent_id) for agent_id in agents}})
-            pp.pprint({"next observations": {f"agent_{agent_id}": simulation.get_observation(agent_id) for agent_id in agents}})
-            pp.pprint({"done": dones})
-        step += 1
-
-
-        if all(dones.values()):
-            break    
+            if all(dones.values()):
+                break    
 
 if __name__=="__main__":
-    from mouse_env_pathmind import MouseAndCheese
+    from pathmind.examples.mouse_env_pathmind import MouseAndCheese
     env = MouseAndCheese()
     rollout_episodes(env)
