93fa72c2b40691989837e365dd6989895e7f492b
Edward Junprung <edward@skymind.io>
2021-11-01 16:18:44 -0700

    Upload factory tutorial example

---
+++ tests/examples/factory/heuristics.py
@@ -0,0 +1,16 @@
+from .base import EnergyFactory
+
+
+class SimpleHeuristic(EnergyFactory):
+
+    def __init__(self, config="./pathmind/factory/config.yaml"):
+        super().__init__(config=config)
+
+        self.prices = self.config.get("price_intervals")
+        self.power_levels = self.config.get('power_levels')
+
+    def set_action(self):
+        self.action = self.power_levels[-1]
+        for i in range(len(self.prices) - 1):
+            if self.prices[i] < self.price <= self.prices[i+1]:
+                self.action = self.power_levels[i]
